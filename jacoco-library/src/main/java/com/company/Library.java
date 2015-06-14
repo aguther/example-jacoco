ac9b15a2114e5c57861a40fbb018a88edb721a85
Andreas Guther <andreas@guther.net>
2015-06-14 17:41:17 +0200

    Added logger to jacoco-library

--- jacoco-library/src/main/java/com/company/Library.java
+++ jacoco-library/src/main/java/com/company/Library.java
@@ -1,17 +1,24 @@
 
 package com.company;
 
+import java.util.logging.Logger;
+
 /**
  * Library that is being used by the application.
  * @author aguther
  */
 public class Library {
 
+  /** Logger for the application. */
+  private static final Logger LOG = Logger.getLogger(Library.class.getName());
+
   /**
    * This method returns true.
    * @return True if valid
    */
   public boolean functionZero() {
+    LOG.fine("functionZero() called");
+
     return true;
   }
 
@@ -22,6 +29,8 @@ public class Library {
    * @return True if valid
    */
   public boolean functionOne(String first) {
+    LOG.fine(String.format("functionOne(first='%s') called", first));
+
     if("One".equals(first)) {
       return true;
     }
@@ -38,6 +47,8 @@ public class Library {
    * @return True if valid
    */
   public boolean functionTwo(String first, String second) {
+    LOG.fine(String.format("functionTwo(first='%s', second='%s') called", first, second));
+
     if("Second".equals(second)) {
       return functionOne(first);
     }
